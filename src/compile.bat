f157632e81a808ec7c3e95f49ce48466e2880047
Erik Yapparov <eeyapparov_1@edu.hse.ru>
2024-05-08 01:44:41 +0300

    Added init

---
+++ src/compile.bat
@@ -0,0 +1,3 @@
+C:\\VulkanSDK\\1.3.275.0\\Bin\\glslc.exe shader.vert -o cmake-build-debug/shaders/vert.spv
+C:\\VulkanSDK\\1.3.275.0\\Bin\\glslc.exe shader.frag -o cmake-build-debug/shaders/frag.spv
+pause
